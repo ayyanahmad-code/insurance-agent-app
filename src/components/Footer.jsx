aaf0d5d0e1d5c914a81c3c6e0f96e64b38e9a8e5
ayyanahmad-code <shaikhayyanahmad@gmail.com>
2025-09-03 11:15:46 +0530

    Remove outdated HTML and JavaScript files for student registration forms; update footer component to include "ayyan" in the copyright notice.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,7 +9,7 @@ export function SimpleFooter() {
         
         {/* Left Side - Brand */}
         <Typography color="white" className="text-lg font-semibold">
-          © {new Date().getFullYear()} Websites Development 🚀
+          © {new Date().getFullYear()} Websites Development ayyan 🚀
         </Typography>
 
         {/* Middle - Navigation */}
